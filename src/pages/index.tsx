8142a61ca4182715bb1d8d45c63a317019d75d42
Jr Bytes <itamazor@gmail.com>
2022-11-03 01:12:33 -0300

    moved title to home page, updated home page to use static props to update data in ten minutes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { FormEvent, useRef } from 'react'
 import Image from 'next/image'
+import { GetStaticProps } from 'next'
 import appPreviewImg from '../assets/app-nlw-copa-preview.png'
 import logoImg from '../assets/logo.svg'
 import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 import iconCheckImg from '../assets/icon-check.svg'
 import { api } from '../lib/axios'
+import Head from 'next/head'
 
 interface HomeProps {
   poolCount: number
@@ -33,72 +35,77 @@ export default function Home(props: HomeProps) {
   }
 
   return (
-    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
-      <main>
-        <Image src={logoImg} alt="Logomarca NLW Copa" />
-        
-        <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>
-          Crie seu próprio bolão da copa e compartilhe entre amigos!
-        </h1>
-        
-        <div className='mt-10 flex items-center gap-2'>
-          <Image src={usersAvatarExampleImg} alt="Preview da aplicação" quality={100} />
-          <strong className='text-gray-100 text-xl'>
-            <span className='text-ignite-500'>+{props.userCount}</span> pessoas já estão usando
-          </strong>
-        </div>
-
-        <form onSubmit={createPool} className='mt-10 flex gap-2'>
-          <input
-            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
-            type="text"
-            required
-            placeholder="Qual nome do seu bolão?"
-            ref={poolTitleRef}
-          />
-          <button
-            className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'
-            type="submit"
-          >
-            Criar meu bolão
-          </button>
-        </form>
-
-        <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
-          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
-        </p>
-
-        <div className='mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100'>
-          <div className='flex items-center gap-6'>
-            <Image src={iconCheckImg} alt="Ícone de check" />
-            <div className='flex flex-col'>
-              <span className='font-bold text-2xl'>+{props.poolCount}</span>
-              <span>Bolões criados</span>
-            </div>
+    <>
+      <Head>
+        <title>NLW Copa</title>
+      </Head>
+      <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
+        <main>
+          <Image src={logoImg} alt="Logomarca NLW Copa" />
+          
+          <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>
+            Crie seu próprio bolão da copa e compartilhe entre amigos!
+          </h1>
+          
+          <div className='mt-10 flex items-center gap-2'>
+            <Image src={usersAvatarExampleImg} alt="Preview da aplicação" quality={100} />
+            <strong className='text-gray-100 text-xl'>
+              <span className='text-ignite-500'>+{props.userCount}</span> pessoas já estão usando
+            </strong>
           </div>
 
-          <div className='w-px h-14 bg-gray-600' />
+          <form onSubmit={createPool} className='mt-10 flex gap-2'>
+            <input
+              className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
+              type="text"
+              required
+              placeholder="Qual nome do seu bolão?"
+              ref={poolTitleRef}
+            />
+            <button
+              className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'
+              type="submit"
+            >
+              Criar meu bolão
+            </button>
+          </form>
 
-          <div className='flex items-center gap-6'>
-            <Image src={iconCheckImg} alt="Ícone de check" />
-            <div className='flex flex-col'>
-              <span className='font-bold text-2xl'>+{props.guessCount}</span>
-              <span>Palpites enviados</span>
+          <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
+            Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+          </p>
+
+          <div className='mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100'>
+            <div className='flex items-center gap-6'>
+              <Image src={iconCheckImg} alt="Ícone de check" />
+              <div className='flex flex-col'>
+                <span className='font-bold text-2xl'>+{props.poolCount}</span>
+                <span>Bolões criados</span>
+              </div>
+            </div>
+
+            <div className='w-px h-14 bg-gray-600' />
+
+            <div className='flex items-center gap-6'>
+              <Image src={iconCheckImg} alt="Ícone de check" />
+              <div className='flex flex-col'>
+                <span className='font-bold text-2xl'>+{props.guessCount}</span>
+                <span>Palpites enviados</span>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
 
-      <Image
-        src={appPreviewImg}
-        alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa"
-        quality={100}
-      />
-    </div>
+        <Image
+          src={appPreviewImg}
+          alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa"
+          quality={100}
+        />
+      </div>
+    </>
   )
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const [
     poolCountResponse,
     guessCountResponse,
@@ -109,11 +116,14 @@ export const getServerSideProps = async () => {
     api.get('users/count'),
   ])
 
+  const tenMinutesInSeconds = 60 * 10
+
   return {
     props: {
       poolCount: poolCountResponse.data.count,
       guessCount: guessCountResponse.data.count,
       userCount: userCountResponse.data.count,
-    }
+    },
+    revalidate: tenMinutesInSeconds,
   }
 }
